678f56d465b2489b92086660f22e75ba10e5ac65
amirgit-p <amirgit84@gmail.com>
2024-12-13 19:13:42 +0330

    A6 blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import category_blog , Blog
 
 # Register your models here.
+
+admin.site.register(category_blog)
+admin.site.register(Blog)
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render ,get_object_or_404
+from .models import Blog
 
 # Create your views here.
 def blog(request):
-    return render(request,'blog/blog.html')
+    blogs = Blog.objects.filter(status = True)
+    context = {
+        "blog" : blogs
+    }
+    return render(request,'blog/blog.html' , context=context)
 
-def blog_detail(request):
-    return render(request,'blog/blog-details.html')
+def blog_detail(request ,id):
+    blog_details = get_object_or_404(Blog , id=id)
+    context = {
+        "blog_details" : blog_details
+    }
+    return render(request,'blog/blog-details.html' , context= context)
