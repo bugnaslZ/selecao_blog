6fcc93855f69792e47eaba604b6e286c2c3a781e
elika2004 <elikatavakoli6561@gmail.com>
2024-12-14 13:14:24 +0330

    update

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
 class category_blog(models.Model):
@@ -10,6 +11,7 @@ class category_blog(models.Model):
     def __str__(self):
         return self.title
 
+    
 class Blog(models.Model):
     user = models.ForeignKey(User , on_delete=models.CASCADE)
     image = models.ImageField(upload_to="blog" , default="default.jpg")
@@ -17,7 +19,6 @@ class Blog(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.BooleanField(default=True)
 
-    def __str__(self):
-        return self.user
+
     
 #____________--___________--
