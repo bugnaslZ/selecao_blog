d533d671d9ad8021e66caaa478870917852e9f81
bugnaslZ <bugnaslz83@gmail.com>
2024-12-14 04:17:07 -0800

    update org

--- blog/views.py
+++ blog/views.py
@@ -1,35 +1,14 @@
 from django.shortcuts import render ,get_object_or_404
 from .models import Blog
-from django.core.paginator import Paginator
 
 # Create your views here.
-def blog(request, category=None):
-    if category is not None:
-        blog = Blog.objects.filter(category__title=category)
-        
-    else:
-        blog = Blog.objects.all()
-        
-    blog_paginate = Paginator(blog, 2)
-    first_page = 1
-    last_page = blog_paginate.num_pages
-
-    try:
-        page_number = request.GET.get("page")
-        blog = blog_paginate.get_page(page_number)
-    except:
-        page_number = first_page
-        blog = blog_paginate.get_page(first_page)
-    
+def blog(request):
+    blogs = Blog.objects.filter(status = True)
     context = {
-        "blogs":blog,
-        "first" : first_page,
-        "last" : last_page
+        "blog" : blogs
     }
+    return render(request,'blog/blog.html' , context=context)
 
-
-    return render(request, 'blog/blog.html', context=context) 
-
-def blog_detail(request):
+def blog_detail(request ):
    
     return render(request,'blog/blog-details.html' )
