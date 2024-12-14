7716eeccdc2e99ef43d6f6f801844e1dfdfdb2f0
YVismeh <Yvisme41148@gmail.com>
2024-12-14 19:43:26 +0330

    end update

--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,6 @@ from django.db import models
 
 # Create your models here.
 
-
-from django.db import models
-
-
-# Create your models here.
-
 class category_blog(models.Model):
     title = models.CharField(max_length=220)
     status = models.BooleanField(default=True)
