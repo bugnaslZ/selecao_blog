b9f58d7a484f6a526e1aa647ed544e782c8dd5d6
Elham Zamani <38107714+Ella56@users.noreply.github.com>
2024-12-15 15:09:52 +0330

    Update views.py

--- root/views.py
+++ root/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from .forms import ContactusForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    return render(request,'root/index.html')
+    if request.method=="POST":
+        form = ContactusForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS," Your contact has been sent successfully")
+            return render(request,"root/index.html")
+        else:
+            messages.add_message(request, messages.ERROR," Your contact info is not valid")
+            return render(request,"root/index.html")
+    else:
+        return render(request,"root/index.html")
 
 def service_detail(request):
     return render(request,'root/service-details.html')
@@ -10,19 +22,5 @@ def service_detail(request):
 def protfolio_detail(request):
     return render(request,'root/portfolio-details.html')
 
-from .forms import ContactusForm
-from django.contrib import messages
 
 
-def contactus(request):
-    if request.method=="POST":
-        form = ContactusForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.add_message(request, messages.SUCCESS," Your contact has been sent successfully")
-            return render(request,"root/contact.html")
-        else:
-            messages.add_message(request, messages.ERROR," Your contact is not valid")
-            return render(request,"root/contact.html")
-    else:
-        return render(request,"root/contact.html")
